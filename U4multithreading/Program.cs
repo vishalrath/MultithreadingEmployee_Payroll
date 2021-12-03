a09fd37db5314044a458bbf4cac58bd9cad834f5
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-03 14:17:41 +0530

    [vishal] Added Multiple Employee with multithread to time

---
+++ U4multithreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace U4multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
