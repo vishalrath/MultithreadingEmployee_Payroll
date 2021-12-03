ec219aca2667073cd69cc0de62bf668149e2d75f
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-03 14:16:03 +0530

    [vishal] Added Multiple Employee with multithread to time

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Uc3EmployeePayrolMultithreading;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<EmployeeDetails> employeeDetails = new List<EmployeeDetails>();
+            employeeDetails.Add(new EmployeeDetails(EmployeeId: 1, EmployeeName: "vishal", PhoneNumber: 123456789, Address: "pusad", Department: "AA", Gender: "M", BasicPay: 2422242, Deduction: 43535, TaxablePay: 234324, IncomeTax: 424, NetPay: 555435)); ;
+            employeeDetails.Add(new EmployeeDetails(EmployeeId: 2, EmployeeName: "Rakesh", PhoneNumber: 123456789, Address: "washim", Department: "AA", Gender: "M", BasicPay: 2422242, Deduction: 43535, TaxablePay: 234324, IncomeTax: 424, NetPay: 555435));
+            employeeDetails.Add(new EmployeeDetails(EmployeeId: 3, EmployeeName: "Amir", PhoneNumber: 123456789, Address: "mumbai", Department: "AA", Gender: "M", BasicPay: 2422242, Deduction: 43535, TaxablePay: 234324, IncomeTax: 424, NetPay: 555435));
+            employeeDetails.Add(new EmployeeDetails(EmployeeId: 4, EmployeeName: "Rajesh", PhoneNumber: 123456789, Address: "Pune", Department: "AA", Gender: "M", BasicPay: 2422242, Deduction: 43535, TaxablePay: 234324, IncomeTax: 424, NetPay: 555435));
+            employeeDetails.Add(new EmployeeDetails(EmployeeId: 5, EmployeeName: "Ramesh", PhoneNumber: 123456789, Address: "Mumbai", Department: "AA", Gender: "M", BasicPay: 2422242, Deduction: 43535, TaxablePay: 234324, IncomeTax: 424, NetPay: 555435)); ;
+
+            EmployeePayrollUsingThreads employeePayrollUsingThreads = new EmployeePayrollUsingThreads();
+            DateTime startDateTime = DateTime.Now;
+            employeePayrollUsingThreads.addEmployeeToPayroll(employeeDetails);
+            DateTime stopDateTime = DateTime.Now;
+            Console.WriteLine("Duration without thread: " + (stopDateTime - startDateTime));
+
+            DateTime startDateTimeThread = DateTime.Now;
+            employeePayrollUsingThreads.addEmployeeToPayrollWithThread(employeeDetails);
+            DateTime stopDateTimeThread = DateTime.Now;
+            Console.WriteLine("Duration with thread: " + (stopDateTimeThread - startDateTimeThread));
+        }
+    }
+}
---
+++ Uc3EmployeePayrolMultithreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uc3EmployeePayrolMultithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
