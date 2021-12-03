2e9b0afa5ca38f13d9511c78642788e9033b5365
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-03 14:09:43 +0530

    [vishal] Added Multiple Employee

---
+++ Uc1AddMultipleEmployee_PayRoll_ultitrading/EmployeePayRollUsingThread.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc1AddMultipleEmployee_PayRoll_ultitrading
+{
+    class EmployeePayRollUsingThread
+    {
+
+        public List<EmployeeDetails> employeeDetailsList = new List<EmployeeDetails>();
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeeDetailsList)
+        {
+            employeeDetailsList.ForEach(employeeData =>
+            {
+                Console.WriteLine(" Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeePayroll(employeeData);
+                Console.WriteLine(" Employee added: " + employeeData.EmployeeName);
+            });
+            Console.WriteLine(this.employeeDetailsList.ToString());
+        }
+
+        public void addEmployeePayroll(EmployeeDetails emp)
+        {
+            employeeDetailsList.Add(emp);
+        }
+    }
+}
---
+++ Uc1AddMultipleEmployee_PayRoll_ultitrading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uc1AddMultipleEmployee_PayRoll_ultitrading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("multithreading!");
+        }
+    }
+}
